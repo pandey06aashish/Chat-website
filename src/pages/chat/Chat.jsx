2401993d5b024fbc12afb5a9c9849ddc3b3c7da8
Aashish Pandey <pandey786aashish@gmail.com>
2024-08-18 16:14:24 +0530

    we will completed chat ui (leftsidebar and chat box bar)

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './chat.css'
+import LeftSidebar from '../../components/LeftSiderbar/LeftSidebar'
+import ChatBox from '../../components/ChatBox/ChatBox'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
 
 const Chat = () => {
   return (
-    <div>
-        
+    <div className='chat'>
+     <div className="chat-container">
+        <LeftSidebar/>
+        <ChatBox/>
+        <RightSidebar/>
+     </div>
     </div>
   )
 }
